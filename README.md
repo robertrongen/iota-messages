fe9d7c59962eaba225b967f9bcdaa1b252d17dba
Robert Rongen <37987463+robertrongen@users.noreply.github.com>
2020-04-18 21:05:29 +0200

    modified:   README.md 	modified:   package-lock.json 	deleted:    package.json 	new file:   src/receive.js 	modified:   src/send.js

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # iotatest
-Test application used in IOTA Academy course
+Test application used in IOTA Academy course.
+Quickstart also available on https://docs.iota.org/docs/client-libraries/0.1/getting-started
+
+## Preparation
+Requires node.js, verify in terminal with node -v
+
+In terminal:
+	npm install @iota/core
+
+ Initialize npm and create a package.json file with the command:
+	npm init -f
+
+## Setup accounts
+Create a seed in GitBash:
+ 	cat /dev/urandom | tr -dc A-Z9 | head -c${1:-81} && echo
+
+address.js: 
+    Replace seed 
+    Run to generate 10 addresses
+
+Send test IOTA tokens to address via https://faucet.comnet.einfachiota.de/#/
+
+## Send transaction
+
+
+Verify transaction: https://utils.iota.org/
+
--- src/send.js
+++ src/send.js
@@ -1,16 +1,34 @@
-const Iota = require('iota/core');
+const Iota = require('@iota/core');
 const Converter = require('@iota/converter');
 const TransactionConverter = require('@iota/transaction-converter');
 
 const iota = Iota.composeAPI({
-    provider: 'https://nodes.devnet.iota.org:443'
-})
+    // provider: 'https://nodes.devnet.iota.org:443'
+    provider: 'https://nodes.devnet.thetangle.org:443'
+    
+});
 
 const seed = 'LKCZPHLLDIYRRJGKLM99ZOP9UICYDPMXROFZQXFHNSLR9JM9ZEUEGZSDMDJ9VJTFKIHIIGFJDNMFQOWPN';
 const outputAddress = 'IXOGDENZIWASTKQSNPMQUDCFQXRMYTWNBVLOFMKFWEZNNBPYHSFGPVIMCRNIUBQQLVBQEEZIQ9XUADGHCHNTBISOV9';
 
 const depth = 3;                // With depth=3 we define that we start the Random Walk, three milestones deep in the tangle
 const minWeightMagnitude = 9;   // minWeightMagnitude=9 defines the difficulty of the PoW
+const tag = 'IOTACADEMY9TUTORIAL9MESSAGE';
+const message = Converter.asciiToTrytes('Hello IOTA!');
 
+// Define the transaction
+const transfers = [
+    {
+        value: 0,
+        address: outputAddress,
+        tag: tag,
+        message: message
+    }
+ ];
 
+// Create the transaction
+iota.prepareTransfers(seed, transfers)
+    .then(trytes => trytes.map(hash => 
+        console.log(TransactionConverter.asTransactionObject(hash)))
+    );
 
